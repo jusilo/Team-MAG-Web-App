481ab703a6449d7d1521e119d3f5dc0727408356
yrrel <fulejohnlerry1112@gmail.com>
2025-03-04 14:02:21 +0800

    added event table in model.py

--- backend/model.py
+++ backend/model.py
@@ -11,3 +11,19 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.email}>'
+
+class UserInfo(db.Model):
+    __tablename__ = 'event'
+    event_id = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String(100), nullable=False)
+    event_creator = db.Column(db.String(100), nullable=False)
+    event_description = db.Column(db.String(10000), nullable=False)
+    event_location = db.Column(db.String(10000), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=True)  # timestamp without time zone
+    last_updated = db.Column(db.DateTime, nullable=True)  # timestamp without time zone
+    event_attendees = db.Column(db.ARRAY(db.Integer), nullable=True)  # integer[]
+    uid = db.Column(db.Integer, nullable=False)  # Foreign key reference (if needed)
+
+    def __repr__(self):
+        return f"<Event {self.event_name}>"
+
