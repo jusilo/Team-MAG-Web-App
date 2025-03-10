a3112268ff0a6bec5eaa24ac8c24094cf0a987dd
yrrel <fulejohnlerry1112@gmail.com>
2025-03-10 09:56:27 +0800

    fix event attendees using UID instead of name

--- backend/fetchAll_event.py
+++ backend/fetchAll_event.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, session
 from app import db
-from .model import Event  # Import the User model from the main app
+from .model import Event,User  # Import the User model from the main app
 
 events_blueprint = Blueprint('events', __name__, template_folder='frontend/templates')
 
@@ -11,7 +11,13 @@ def home_page():
     
     # Fetch all events from the database (no pagination)
     events = Event.query.all()  # Fetch all records from the event table
-    
+    #to print names instead of UID
+    for event in events:
+        attendees = event.get_attendee_users()  
+        print(f"Attendees for {event.event_name}: {attendees}")
+    print(events)  # Should print <Event some_event_name>
+    print(type(events))  # Should print <class 'your_flask_app.models.Event'>
     # Render the template and pass the events to it
-    return render_template('home.html', events=events)
+    return render_template('home.html', events=events, attendees=attendees)
+
 
